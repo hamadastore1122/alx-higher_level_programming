32b1a7a982d22f3a59dc9032d12c94cc3b854e2e
hamadastore1122 <shehabstore22@gmail.com.com>
2023-11-27 14:25:16 -0800

    update

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "School1"
-str1 = str1  + " " + str2
-print("Welcome to {}!".fotmat(str1))
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
