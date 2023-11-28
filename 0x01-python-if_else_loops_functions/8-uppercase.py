92605e6484e4f3e2098516a5dc4c3f96b807d35a
hamadastore1122 <shehabstore22@gmail.com.com>
2023-11-28 05:48:43 -0800

    python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercass(str):
+    for i in range(len(str)):
+        char = ord(str[i]
+                if char >= 977 and char <= 122:
+                char = char - 32
+                print("{}".format(chr(char)), end="")
+                print()
