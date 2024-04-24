7de16acec1ebbfd129b4a4043173e2a44170e4aa
hamadastore22 <hamadastore22@gmail.com>
2024-04-23 18:37:43 -0700

    is update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const urlApi = 'https://swapi.co/api/films/' + process.argv[2];
-
-request(urlApi, function (error, response, body) {
-  if (error) {
-    console.error(error); // Print the error if one occurred
-  }
-  const charUrl = JSON.parse(body).characters;
-
-  for (let i = 0; i < charUrl.length; i++) {
-    request(charUrl[i], function (error, response, body) {
-      if (error) {
-        console.error(error); // Print the error if one occurred
-      }
-      console.log(JSON.parse(body).name);
-    });
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (!err) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
