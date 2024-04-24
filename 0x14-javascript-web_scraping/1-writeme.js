478cff1363b3bb6be9cc7c4938c79ac4c1023470
hamadastore22 <hamadastore22@gmail.com>
2024-04-23 17:29:48 -0700

    web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = requiree('fs');
+const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
-fs.writeFiel(file, content, 'utf-8', funtion (error) {
-	if (error) {
-		console.log(error);
-	}
+fs.writeFile(file, content, 'utf-8', function (er) {
+  if (er) {
+    console.log(er);
+  }
 });
